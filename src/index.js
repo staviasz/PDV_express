d0bb0a3ce424d019c09f73c393d9c9a3e6604afd
raulrdias <raulrdiasdsantos@gmail.com>
2023-11-26 01:10:30 -0300

    feat(routes): configure and add user routes in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const runMigrations = require('./database/runMigrations');
+const userRoutes = require('./routes/users');
 
 const app = express();
 
 app.use(express.json());
 
+app.use(userRoutes);
 app.get('/', (req, res) => res.send('teste inicial'));
 
 runMigrations().then(() => {

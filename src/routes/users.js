694363e36fae185905434c2b0838462e2bc80308
raulrdias <raulrdiasdsantos@gmail.com>
2023-11-26 01:07:11 -0300

    feat(routes): add user creation endpoint

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,1 +1,8 @@
-module.exports = {};
+const { Router } = require('express');
+const { createUser } = require('../controllers/users')
+
+const routes = Router();
+
+routes.post('/usuario', createUser);
+
+module.exports = routes;

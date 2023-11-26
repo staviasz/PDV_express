45185a4889a39e1ec76abc7a70d7c0a86ff571d4
Caique Castro <caiquecastrosilva@gmail.com>
2023-11-26 17:23:07 -0300

    Verify Login functionality implemented

--- src/middlewares/authenticated.js
+++ src/middlewares/authenticated.js
@@ -1,1 +1,39 @@
-module.exports = {};
+require('dotenv').config();
+
+const jwt = require('jsonwebtoken');
+const db = require('knex')(require('../../knexfile'));
+const error = require('../utils/responses/errorResponse');
+
+const verifyLogin = async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return error.errorResponse401(res, 'Usuario Não autorizado');
+  }
+
+  try {
+    const token = authorization.replace('Bearer ', '').trim();
+
+    const { id } = jwt.verify(token, process.env.SECRET_JWT);
+
+    const dataUser = await db('usuarios').where({ id }).first();
+
+    if (!dataUser) {
+      return error.errorResponse404(res, 'Usuario não encontrado');
+    }
+
+    const user = {
+      id: dataUser.id,
+      nome: dataUser.nome,
+      email: dataUser.email,
+    };
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    return error.errorResponse401(res, 'Usuario Não autorizado');
+  }
+};
+
+module.exports = { verifyLogin };
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
 
 const users = require('../controllers/users');
+const { verifyLogin } = require('../middlewares/authenticated');
 
 const routes = Router();
 
 routes.put('/login', users.login);
+
+routes.use(verifyLogin);
 
 module.exports = routes;

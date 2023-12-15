84e413216acf8fdc40af860876de6e199e59c7c8
staviasz <erickesc21@hotmail.com>
2023-12-14 22:23:47 -0300

    solved invalid url of aws

--- src/configs/aws.js
+++ src/configs/aws.js
@@ -3,7 +3,7 @@ const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
 const s3Client = new S3Client({
   region: process.env.BUCKET_REGION,
-  endpoint: process.env.ENDPOINT_S3,
+  endpoint: `https://${process.env.ENDPOINT_S3}`,
   credentials: {
     accessKeyId: process.env.BUCKET_KEY,
     secretAccessKey: process.env.BUCKET_APP_KEY,
@@ -21,7 +21,7 @@ const upload = async (originalname, buffer, mimetype) => {
   const command = new PutObjectCommand(params);
 
   await s3Client.send(command);
-  return `https://${process.env.BUCKET_NAME}${process.env.ENDPOINT_RETURN}/${originalname}`;
+  return `https://${process.env.BUCKET_NAME}.${process.env.ENDPOINT_S3}/${originalname}`;
 };
 
 module.exports = { upload };
